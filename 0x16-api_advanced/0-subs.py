82647eb0d16ecba844ae260f2ef093155b258757
Konyuka <michaelsaiba84@gmail.com>
2024-06-20 07:37:49 -0700

    added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script to query the Reddit API and return the number of subscribers for a given subreddit.
+Sript to query the Reddit API and return the number of subscribers for a given subreddit.
 """
 
 import requests
