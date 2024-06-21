b2a99c80d6b2c94653db2e30d9f16afc80ca2bb4
Konyuka <michaelsaiba84@gmail.com>
2024-06-20 23:31:49 -0700

    Pushed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and returns the number of subscribers"""
+"""A Function that queries the Reddit API and returns the number of subscribers"""
 import requests
 
 
