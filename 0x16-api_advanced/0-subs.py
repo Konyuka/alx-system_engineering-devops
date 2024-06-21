9482d7e7aa582ad2e23595353835487e2d1d03b2
Michael Saiba <michaelsaiba84@gmail.com>
2024-06-21 09:45:36 +0300

    ck

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,10 @@
 Importing requests module
 """
 
-
 from requests import get
 
 
 def number_of_subscribers(subreddit):
-    
     """
     function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
